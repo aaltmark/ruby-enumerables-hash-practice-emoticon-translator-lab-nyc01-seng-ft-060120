e663504356dade06ab56979a69b185b88192fee9
Adam Altmark <adamaltmark@gmail.com>
2020-05-18 18:18:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,60 @@
 # require modules here
 
 def load_library
-  # code goes here
+  emoticons = {
+    :angel => {
+      :english => "O:)",
+      :japanese => "☜(⌒▽⌒)☞",
+    },
+    :angry => {
+      :english => ">:(",
+      :japanese => "ヽ(ｏ`皿′ｏ)ﾉ",
+    },
+    :bored => {
+      :english => ">:(",
+      :japanese => "(ΘεΘ;)",
+    },
+    :confused => {
+      :english => "%)",
+      :japanese => "(゜.゜)",
+    },
+    :embarrassed => {
+      :english => ":$",
+      :japanese => "(#^.^#)",
+    },
+    :fish => {
+      :english => "><>",
+      :japanese => ">゜))))彡",
+    },
+    :glasses => {
+      :english => "8D",
+      :japanese => "(^0_0^)",
+    },
+    :grinning => {
+      :english => "=D",
+      :japanese => "(￣ー￣)",
+    },
+    :happy => {
+      :english => ":)",
+      :japanese => "(＾ｖ＾)",
+    },
+    :kiss => {
+      :english => ":*",
+      :japanese => "(*^3^)/~☆",
+    },
+    :sad => {
+      :english => ":'(",
+      :japanese => "(Ｔ▽Ｔ)",
+    },
+    :surprised => {
+      :english => ":o",
+      :japanese => "o_O",
+    },
+    :wink => {
+      :english => ";)",
+      :japanese => "(^_-)",
+    },
+  }
 end
 
 def get_japanese_emoticon
