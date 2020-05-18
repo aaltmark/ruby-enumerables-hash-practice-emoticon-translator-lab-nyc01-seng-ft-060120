7f2deb5d0889531cbd2d863d74c20145a9d7fa6f
Adam Altmark <adamaltmark@gmail.com>
2020-05-18 19:53:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ end
 
 def get_japanese_emoticon (path, emoticon)
   new_hash = load_library(path)
+  japanese_emoticon
   new_hash.each do |emotion, emoticon_hash|
     if emoticon_hash][:english] == emoticon
       japanese_emoticon = emoticon_hash[:japanese]
